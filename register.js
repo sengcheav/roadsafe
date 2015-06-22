366659d92dda46052c0ad4e43f36cd852400ce62
Seng Leat <leatseng@caprera.ecs.vuw.ac.nz>
2015-06-22 15:20:07 +1200

    addon users

--- register.js
+++ register.js
@@ -12,8 +12,8 @@ var un = 'test';
 var pw = '1234';
 
 //CREATE A SCHEMA - users
-//query = client.query('DROP TABLE users;');
-query = client.query('CREATE TABLE users(username text PRIMARY KEY, password text NOT NULL, token text DEFAULT "absent")');
+query = client.query('DROP TABLE users;');
+query = client.query('CREATE TABLE users(username text PRIMARY KEY, password text NOT NULL, token text DEFAULT 'absent')');
 
 //CALLBACK TO END DATABASE CONNECTION
 query.on('end', function(result) { client.end(); });
