a18c871c4eace1be524f1698f03b8ce50673f9db
Seng Leat <leatseng@barretts.ecs.vuw.ac.nz>
2015-06-22 23:04:20 +1200

    add token table

--- register.js
+++ register.js
@@ -12,8 +12,8 @@ var un = 'test';
 var pw = '1234';
 var absent = 'absent' ;
 //CREATE A SCHEMA - users
-query = client.query('DROP TABLE users;');
-query = client.query('CREATE TABLE users(username text PRIMARY KEY, password text NOT NULL, token text DEFAULT "absent")');
-
+//query = client.query('DROP TABLE users;');
+//query = client.query('CREATE TABLE users(username text PRIMARY KEY, password text NOT NULL, token text DEFAULT "absent")');
+query = client.query(' CREATE TABLE rank (USERNAME VARCHAR(15) PRIMARY KEY, TOTALPOINTS INTEGER DEFAULT 0 CHECK (TOTALPOINTS >=0 ) , POINTS_LVL INTEGER[10] , LVL_BEST INTEGER[10] ) ') ;
 //CALLBACK TO END DATABASE CONNECTION
 query.on('end', function(result) { client.end(); });
